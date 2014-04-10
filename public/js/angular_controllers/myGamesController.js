9e1ea6c900cf8270f3dcb6538d895d37b282e750
Hack Reactor Students <accounts@hackreactor.com>
2014-04-10 14:04:43 -0700

    Finished join game, needs db function to update user to work.

--- public/js/angular_controllers/myGamesController.js
+++ public/js/angular_controllers/myGamesController.js
@@ -3,24 +3,9 @@ angular.module('angularMyGames', [])
     $scope.showUpcoming = true;
     $scope.showPlayed = true;
     $scope.getMyGames = function () {
-      $rootScope.currentUser.gamesPlayed.push('sample played game');
       $scope.myUpcomingGames = $rootScope.currentUser.upcomingGames;
       $scope.myPlayedGames = $rootScope.currentUser.gamesPlayed;
     };
     $scope.getMyGames();
   }]);
 
-
-  // .factory('angularGetMyGames', ['$http', function($http) {
-  //   return {
-  //     get: function(cb) {
-  //       var getGames = $http.get('/game', cb);
-  //       getGames.success(function(data) {
-  //         cb(data);
-  //       });
-  //       getGames.error(function() {
-  //         cb('Something went wrong, please try again');
-  //       });
-  //     }
-  //   };
-  // }]);
--- public/js/angular_controllers/seeGameController.js
+++ public/js/angular_controllers/seeGameController.js
@@ -1,5 +1,5 @@
 angular.module('angularSeeGame', [])
-  .controller('seeGameController', ['$rootScope', '$scope', 'angularPutGame', function($rootScope, $scope, angularPutGame) {
+  .controller('seeGameController', ['$rootScope', '$scope', 'angularPutGame', 'angularPutUser', function($rootScope, $scope, angularPutGame, angularPutUser) {
     $scope.addToGame = function() {
       angularPutGame.put($scope.game, function(result, response) {
         $scope.response = response;
@@ -8,12 +8,26 @@ angular.module('angularSeeGame', [])
         }
       });
     };
+
+    $scope.addGameToUser = function() {
+      angularPutGame.put($scope.game, function(result, response) {
+        $scope.response = response;
+        if (result === 'success') {
+          $rootScope.currentUser.upcomingGames.push($scope.game.code);
+          angularPutUser.put($rootScope.currentUser, function(data) {
+            console.log(data);
+          });
+        }
+      });
+    };
+
     $scope.joinGame = function () {
       console.log('joined!');
       $scope.game = {};
       $scope.game.code = 123;
       $scope.game.phone = $rootScope.currentUser.phone;
       $scope.addToGame();
+      $scope.addGameToUser();
     };
 
   }])
@@ -28,6 +42,20 @@ angular.module('angularSeeGame', [])
         });
         putGame.error(function() {
           cb('error', 'Something went wrong, please try again');
+        });
+      }
+    };
+  }])
+
+  .factory('angularPutUser', ['$http', function($http) {
+    return {
+      put: function(userData, cb) {
+        var putUser = $http.put('/users', userData, cb);
+        putUser.success(function(data) {
+          cb(data);
+        });
+        putUser.error(function() {
+          cb('Something went wrong, please try again');
         });
       }
     };
