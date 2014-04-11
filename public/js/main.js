62b0fd275f0a2eec4d17dbde9edc181b3cb9da7e
Andrew Magliozzi <andrew.magliozzi@gmail.com>
2014-04-11 13:06:02 -0400

    brining in public files

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  /* Applies class to n children (better than CSS3 for IE compatibility) */
+  $('.service:nth-child(4n+1)').addClass('no-left-margin');
+
+  // Game dropdown on homepage
+  $('.chosen-select').chosen();
+
+  $( ".datepicker" ).datepicker();
+
+});
