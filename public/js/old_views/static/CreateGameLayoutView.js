36b2ba818a7fdd7a80ae834f0c2158de0ec61169
Adrian Kim <uknoiluv@gmail.com>
2014-04-13 09:40:36 -0700

    4/13 join a game restriction if game wasnt pick

--- public/js/old_views/static/CreateGameLayoutView.js
+++
@@ -1,8 +0,0 @@
-CreateGameLayoutView = Backbone.Marionette.Layout.extend({
-  template: "#create-gameLayoutView",
-
-  regions: {
-    gmap: "#map-canvas",
-    gameCreate: '#createGame'
-  }
-});
--- public/js/old_views/static/GameLayoutView.js
+++
@@ -1,9 +0,0 @@
-GameLayoutView = Backbone.Marionette.Layout.extend({
-  template: "#layout-template",
-  className: 'span10 offset1',
-
-  regions: {
-    gmap: "#map-canvas",
-    game: '#game'
-  }
-});
