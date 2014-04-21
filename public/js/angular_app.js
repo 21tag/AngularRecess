586e8ecb66dfa6189cab7849a1da4d9877bee170
emilycoco <emilymcoco@gmail.com>
2014-04-21 14:22:35 -0700

    added marker to see game page.

--- public/js/angular_app.js
+++ public/js/angular_app.js
@@ -12,7 +12,7 @@ angular.module('angularApp', ['angularAppRoutes', 'angularLogin', 'angularSignup
             return $rootScope.currentUser;
           } else {
             //checks route on refresh
-            var restricted = ['/listAGame', '/myGames', '/userProfile'];
+            var restricted = ['/listAGame', '/myGames', '/seeGame', '/userProfile'];
             if (_.contains(restricted, $location.$$path)) {
               $state.go('login');
             }
