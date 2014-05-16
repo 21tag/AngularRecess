3d69848d427b8b0a9506df2c8924487d0b559238
emilycoco <emilymcoco@gmail.com>
2014-05-15 20:44:42 -0700

    Added searchability by game type to home page map, added unstyled popups to markers.

--- public/js/angular_controllers/findGamesController.js
+++ public/js/angular_controllers/findGamesController.js
@@ -1,6 +1,7 @@
 angular.module('angularFindGames', [])
 .controller('findGamesController', ['$scope', '$rootScope', '$location', 'angularGames', 'googleMapsFind', function($scope, $rootScope, $location, angularGames, googleMapsFind) {
 
+//gives maps atrributes to controller's scope
   angular.extend($scope, {
     map: {
       control: {},
@@ -10,13 +11,19 @@ angular.module('angularFindGames', [])
         longitude: null
       },
       options: {
-          streetViewControl: false,
-          panControl: true,
-          maxZoom: 20,
-          minZoom: 3,
-          scrollwheel: false
+        streetViewControl: false,
+        panControl: true,
+        maxZoom: 20,
+        minZoom: 3,
+        scrollwheel: false
       },
       dragging: true,
+      infoWindow: {
+        options: {
+          disableAutoPan: true
+        },
+        show: true
+      },
       bounds: {},
       events: {
         tilesloaded: function (mapModel, eventName, originalEventArgs) {
@@ -27,8 +34,7 @@ angular.module('angularFindGames', [])
     }
   });
 
-  $scope.map.markers = [];
-
+//finds user and centers map on their location as default
   $scope.mapUser = function() {
     navigator.geolocation.getCurrentPosition(function(position) {
       $scope.map.center.latitude = position.coords.latitude;
@@ -39,7 +45,10 @@ angular.module('angularFindGames', [])
 
   $scope.mapUser();
 
+//searches for game and location by calling the Googlemaps geocode api
   $scope.findLocation = function(gameType, location) {
+    gameType = gameType || 'All Games';
+    location = location || 'San Francisco';
     googleMapsFind.get('http://maps.googleapis.com/maps/api/geocode/json?address=' + location + '&sensor=true', function(data) {
       var foundLocation = data.results[0].geometry.location;
       $scope.map.center.latitude = foundLocation.lat;
@@ -48,23 +57,20 @@ angular.module('angularFindGames', [])
     });
   };
 
-  $scope.game = [];
+//declaring markers here prevents error on map load if retrieveGames function does not return before map load
+  $scope.map.markers = [];
 
-  $scope.filterOrg = function(data){
-    return !_.contains($rootScope.currentUser.upcomingGames, data._id);
-  };
-
+//fetches games, and adds them to the map if their gameType matches the searched game.
   $scope.retreiveGames = function(gameSearched){
+    $scope.map.markers = [];
     angularGames.get('/games', function(data) {
       $scope.game = data;
-      _.each($scope.game, function(game, i) {
-        if (game.coord && gameSearched === 'all'){
-          $scope.map.markers.push({latitude: game.coord.lat, longitude: game.coord.lon});
+      _.each($scope.game, function(game) {
+        if (game.coord && gameSearched === 'All Games'){
+          $scope.map.markers.push({latitude: game.coord.lat, longitude: game.coord.lon, name: game.gameName, description: game.gameDescription});
         }
         else if (game.coord && gameSearched === game.gameType) {
-          $scope.map.markers = [];
-          $scope.map.markers.push({latitude: game.coord.lat, longitude: game.coord.lon});
-          console.log($scope.map.markers);
+          $scope.map.markers.push({latitude: game.coord.lat, longitude: game.coord.lon, name: game.gameName, description: game.gameDescription});
         }
       });
       _.each($scope.game, function(item, index){
@@ -73,14 +79,19 @@ angular.module('angularFindGames', [])
     });
   };
 
+//initializes markers for all games on map.
+  $scope.retreiveGames('All Games');
+
   $scope.selectGame = function(data, index){
-    console.log($rootScope.joinGameList);
     $rootScope.joinGameList = undefined;
     $rootScope.joinGameList = data;
     $location.path('/seeGame');
   };
 
-  $scope.retreiveGames('all');
+  $scope.filterOrg = function(data){
+    return !_.contains($rootScope.currentUser.upcomingGames, data._id);
+  };
+
 }])
 
 .factory('angularGames', ['$http', function($http){
@@ -100,7 +111,6 @@ angular.module('angularFindGames', [])
 .factory('googleMapsFind', ['$http', function($http){
   return{
     get: function(url ,cb) {
-      console.log(url);
       var getData = $http.get(url);
       getData.success(function(data) {
         cb(data);
